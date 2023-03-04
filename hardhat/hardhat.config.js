87f7a2d9f9b3208839544778d2c1d98009bbeb26
MehakB7 <87482443+MehakB7@users.noreply.github.com>
2023-03-04 12:54:44 +0530

    updated hardhar configure  file

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config({ path: ".env" });
 const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
